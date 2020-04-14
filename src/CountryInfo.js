bbd93ff1d94258d46b564c918e66f858c3ae2b50
Tiago Baptista Coelho <tiagojnbaptistacoelho@gmail.com>
2020-04-14 17:19:00 +0100

    added some error managing with conditional rendering to the counties info

--- src/CountryInfo.js
+++ src/CountryInfo.js
@@ -11,13 +11,13 @@ class CountryInfo extends Component{
                 <img src={flag} alt={`${name} flag`}/>
                 <p><span>NAME:</span> {name}</p>
                 <p><span>CAPITAL:</span> {capital !== ""? capital: "N/A"}</p>
-                <p><span>LANGUAGE:</span> {languages}</p>
-                <p><span>POPULATION:</span> {population}</p>
-                <p><span>AREA:</span> {area} square metres</p>
-                <p><span>CURRENCY:</span> {currency}</p>
-                <p><span>TIMEZONE:</span> {timezone.length >1 ? timezone.join("/ "): timezone}</p>
-                <p><span>REGION:</span> {region}</p>
-                <p><span>SUBREGION:</span> {subregion}</p>
+                <p><span>LANGUAGE:</span> {languages !== ""? languages: "N/A"}</p>
+                <p><span>POPULATION:</span> {population !== ""? population: "N/A"}</p>
+                <p><span>AREA:</span> {area !== ""? `${area} square metres` : "N/A"}</p>
+                <p><span>CURRENCY:</span> {currency !== ""? currency: "N/A"}</p>
+                <p><span>TIMEZONE:</span> {timezone !== ""? timezone.length >1 ? timezone.join("/ "): timezone : "N/A"}</p>
+                <p><span>REGION:</span> {region !== ""? region: "N/A"}</p>
+                <p><span>SUBREGION:</span> {subregion !== ""? subregion: "N/A"}</p>
             </div>
 
         )
